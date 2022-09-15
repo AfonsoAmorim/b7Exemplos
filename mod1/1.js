e87f28792b3ef1477ac96c054b4d810e124ec313
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-09-15 19:33:52 -0300

    exemplos functions

--- mod1/1.js
+++ mod1/1.js
@@ -5,18 +5,63 @@ function olaGalera() {
 }
 console.log(olaGalera());
 
-function maiorDeIdade(idade){
-    if (idade >18) {
+function maiorDeIdade(idade) {
+    if (idade > 18) {
         return "maior de idade"
-    }else{
+    } else {
         return "menor de idade"
     }
 }
 console.log(maiorDeIdade(22));
 //----------- exercício 
 // Calcule a porcentagem entre 2 números. 
-function porcentagem(a,b){
-    let calculo = (a/b)/100
+function porcentagem(a, b) {
+    let calculo = (a / b) * 100
     console.log(`A porcentagem é ${calculo}`);
 }
-porcentagem(10,5);
+porcentagem(40, 10);
+
+//Calcular o preço do imóvel 
+// m2 - 3000
+// Se tiver 1 quarto, m2 é 1x
+// Se tiver 2 quarto, m2 é 1.2x
+// Se tiver 3 quarto, m2 é 1.5x
+
+let metroQuadradoPreco = 3000
+function calculoMetragem(metros, quartos) {
+    if (quartos === 1) {
+        return `O valor é: ${metroQuadradoPreco * metros}`
+    } else if (quartos == 2) {
+        return `O valor é: ${metroQuadradoPreco * (metros * 1.2)}`
+    } else {
+        return `O valor é: ${metroQuadradoPreco * (metros * 1.5)}`
+    }
+}
+console.log(calculoMetragem(123, 1));
+console.log(calculoMetragem(123, 2));
+console.log(calculoMetragem(123, 3));
+
+//verificar se uduario e senha estão certos
+let usuarioFinal = "Afonso"
+let senhaFinal = 111
+function validador(nomeUsuario, senhaUsuario) {
+    if (nomeUsuario === usuarioFinal && senhaUsuario === senhaFinal) {
+        return "Liberar acesso"
+    } else {
+        return "Acesso negado!"
+    }
+}
+console.log(validador("Afonso",111));
+console.log(validador("Afonso44",44));
+
+//arrow function 
+let soma = (a,b) => {
+    return a+b
+}
+console.log("Usando Arrow function: " + soma(10,5));
+
+let arrowUmParametro = parametro => "Arrow " + parametro;
+console.log(arrowUmParametro("qualquer coisa em um parametro"));
+
+
+
